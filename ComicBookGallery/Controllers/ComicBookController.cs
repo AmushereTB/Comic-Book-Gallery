bc1489080269e8ba0b25de49abffb3c583899ae5
Tianbiao Feng <tianbiaofeng@gmail.com>
2021-11-24 19:45:49 +0100

    added design files

--- ComicBookGallery/Controllers/ComicBookController.cs
+++ ComicBookGallery/Controllers/ComicBookController.cs
@@ -10,7 +10,21 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+
+            ViewBag.SeriesTitle = "Spider Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artist = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+            ViewBag.Warning = "there is no artist in List";
             return View();
+
         }
     }
 }
