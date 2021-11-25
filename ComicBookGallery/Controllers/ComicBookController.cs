5f865ed77aa370630a5f191a7a3573a84f52fed6
Tianbiao Feng <tianbiaofeng@gmail.com>
2021-11-25 14:42:59 +0100

    add Index view file. finished bookrepository

--- ComicBookGallery/Controllers/ComicBookController.cs
+++ ComicBookGallery/Controllers/ComicBookController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,25 +10,26 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBookController : Controller
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBookController()
         {
-            var comicBook = new ComicBook()
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+
+            return View(comicBooks);
+        }
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
             {
-                SeriesTitle = "The Amazing Spider Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! " +
-                                 "Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Warning = "there is no artist in List",
-                Artists = new Artist[] {
-                    new Artist(){Name = "Den", Role ="Script"},
-                    new Artist(){Name = "Humberto", Role ="Pencils"},
-                    new Artist(){Name = "Edger", Role ="Inks"},
-                    new Artist(){Name = "Jessy", Role ="Colors"},
-                    new Artist(){Name = "Peter", Role ="Letters"},
-                }
-
-            };
-
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook(id.Value);
 
             return View(comicBook);
 
